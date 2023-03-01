661ab40324c5e280c3491319b707e24f4fb1f5d7
Marco Sousa <marcosousacontato@gmail.com>
2023-03-01 13:01:26 -0300

    Criando primeiros métodos da API

---
+++ back/src/ProAtividade.API/ProAtividade.API/Controllers/AtividadeController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ProAtividade.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AtividadeController : ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "Primeiro método get"; 
+        }
+
+        [HttpPost]
+        public string Post()
+        {
+            return "Primeiro método post";
+        }
+
+        [HttpPut]
+        public string Put()
+        {
+            return "Primeiro método put";
+        }
+
+        [HttpDelete]
+        public string Delete()
+        {
+            return "Primeiro método delete";
+        }
+    }
+}
