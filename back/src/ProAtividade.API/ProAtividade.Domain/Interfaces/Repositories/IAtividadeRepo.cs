37865a4b3f89965d72243afd670163e9050c27d8
Marco Sousa <marcosousacontato@gmail.com>
2023-03-14 15:05:21 -0300

    Criando interfaces de repositories - domain

---
+++ back/src/ProAtividade.API/ProAtividade.Domain/Interfaces/Repositories/IAtividadeRepo.cs
@@ -0,0 +1,16 @@
+﻿using ProAtividade.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProAtividade.Domain.Interfaces.Repositories
+{
+    public interface IAtividadeRepo
+    {
+        Task<IEnumerable<Atividade>> PegarTodasAsync();
+        Task<IEnumerable<Atividade>> PegarPorIdAsync();
+        Task<IEnumerable<Atividade>> PegarPorTituloAsync();
+    }
+}
