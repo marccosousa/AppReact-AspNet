f9cb3334e955728cd9ef2c15e9e031f2b9703189
Marco Sousa <marcosousacontato@gmail.com>
2023-03-14 15:25:18 -0300

    Começando a implementar AtividadeService

---
+++ back/src/ProAtividade.API/ProAtividade.Domain/Interfaces/Services/IAtividadeService.cs
@@ -0,0 +1,19 @@
+﻿using ProAtividade.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProAtividade.Domain.Interfaces.Services
+{
+    public interface IAtividadeService
+    {
+        Task<Atividade> AdicionarAtividade(Atividade model);
+        Task<Atividade> Atualizar(Atividade model);
+        Task<bool> DeletarAtividade(int AtividadeId);
+        Task<bool> ConcluirAtividade(int AtividadeId);
+        Task<IEnumerable<Atividade>> PegarTodasAtividadesAsync(Atividade model);
+        Task<Atividade> PegarAtividadePorIdAsync(int AtividadeId; 
+    }
+}
---
+++ back/src/ProAtividade.API/ProAtividade.Domain/Services/AtividadeService.cs
@@ -0,0 +1,52 @@
+﻿using ProAtividade.Domain.Entities;
+using ProAtividade.Domain.Interfaces.Repositories;
+using ProAtividade.Domain.Interfaces.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProAtividade.Domain.Services
+{
+    public class AtividadeService : IAtividadeService
+    {
+        private readonly IAtividadeRepo _atividade;
+        private readonly IGeralRepo _geralRepo;
+
+        public AtividadeService(IAtividadeRepo atividadeRepo, IGeralRepo geralRepo)
+        {
+            _atividade = atividadeRepo;
+            _geralRepo = geralRepo;
+        }
+        public Task<Atividade> AdicionarAtividade(Atividade model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Atividade> Atualizar(Atividade model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> ConcluirAtividade(int AtividadeId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DeletarAtividade(int AtividadeId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Atividade> PegarAtividadePorIdAsync(int AtividadeId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Atividade>> PegarTodasAtividadesAsync(Atividade model)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
